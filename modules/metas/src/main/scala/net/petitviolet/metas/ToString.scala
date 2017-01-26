636489a98017b863ff409d0725d39478ff88e6e3
petitviolet <violethero0820@gmail.com>
2017-01-27 00:14:45 +0900

    implemented @ToString annotation

---
+++ modules/metas/src/main/scala/net/petitviolet/metas/ToString.scala
@@ -0,0 +1,53 @@
+package net.petitviolet.metas
+
+import scala.collection.immutable.Seq
+import scala.meta.Ctor.Ref.Name
+import scala.meta.Term.Param
+import scala.meta._
+
+class ToString extends scala.annotation.StaticAnnotation {
+  inline def apply(defn: Any): Any = meta {
+    def createToString(name: Type.Name, paramss: Seq[Seq[Term.Param]]): Defn.Def = {
+
+      val args: Seq[String] = paramss.flatMap { params: Seq[Param] =>
+        params.map { param: Param =>
+          // as just a string like `"n"`
+          val paramName = s""""${param.name}""""
+          // as a term like `n.toString`
+          val value = s"${param.name}.toString".parse[Term].get
+
+          // "\"n\" + \":\" + n.toString"
+          s"""$paramName + ":" + $value"""
+        }
+      }
+
+      // "\"n\" + \":\" + n.toString" +
+      // ", " +
+      // "\"x\" + \":\" + x.toString"
+      val joinedParamStrings: Term = args
+        .mkString(""" + ", " + """)
+        .parse[Term].get
+
+      q"""
+       override def toString: String = {
+         ${name.syntax} + "(" + $joinedParamStrings + ")"
+       }
+      """
+    }
+    defn match {
+      case cls @ Defn.Class(_, name, _, ctor, template) =>
+        val toStringMethod: Defn.Def = createToString(name, ctor.paramss)
+        val templateStats: Seq[Stat] =
+          if (template.syntax.contains("toString")) {
+            template.stats getOrElse Nil
+          } else {
+            toStringMethod +: template.stats.getOrElse(Nil)
+          }
+
+        cls.copy(templ = template.copy(stats = Some(templateStats)))
+      case _ =>
+        println(defn.structure)
+        abort("@ToString must annotate a class.")
+    }
+  }
+}
