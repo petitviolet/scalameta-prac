7fb5ecadbce74abb0b88c1aead356e644c9af10c
petitviolet <violethero0820@gmail.com>
2017-02-05 22:35:26 +0900

    some fix sample

--- modules/app/src/main/scala/net/petitviolet/metas/app/AddFieldApp.scala
+++ modules/app/src/main/scala/net/petitviolet/metas/app/AddFieldApp.scala
@@ -1,38 +1,26 @@
 package net.petitviolet.metas.app
 
-import net.petitviolet.metas.{MixIn, Uses}
+import net.petitviolet.metas.{ MixIn, Uses }
 
-//@AddField
-trait AddFieldApp[Service] {
-}
-
-trait MyService {
-  def double(n: Int) = n * 2
-}
-
-trait OtherService {
-  def triple(n: Int) = n * 3
-}
-
-class OtherServiceImpl extends OtherService
-
+trait MyService { def double(n: Int) = n * 2 }
 object MyServiceImpl extends MyService
 
-@Uses[OtherService]
-@Uses[MyService]
+trait OtherService { def triple(n: Int) = n * 3 }
+class OtherServiceImpl extends OtherService
+
+@Uses[MyService]()
+@Uses[OtherService]()
 trait UsesFieldTarget {
-  def show(n: Int) = println(this.myService.double(n))
+  def showDouble(n: Int) = println(this.myService.double(n))
   def showTriple(n: Int) = println(this.otherService.triple(n))
 }
 
 @MixIn[MyService](MyServiceImpl)
 @MixIn[OtherService](new OtherServiceImpl)
-class UsesFieldTargetImpl extends UsesFieldTarget {
-}
+class UsesFieldTargetImpl extends UsesFieldTarget
 
 object UsesFieldApp extends App {
   val impl = new UsesFieldTargetImpl
-  impl.show(100)
-
-  impl.showTriple(100)
+  impl.showDouble(100) // 200
+  impl.showTriple(100) // 300
 }
