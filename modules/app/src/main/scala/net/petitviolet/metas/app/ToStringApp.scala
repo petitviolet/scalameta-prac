7b7c806654cce851688cabab06ecea258f190953
petitviolet <violethero0820@gmail.com>
2017-02-05 11:26:27 +0900

    add ToString sample

--- modules/app/src/main/scala/net/petitviolet/metas/app/ToStringApp.scala
+++ modules/app/src/main/scala/net/petitviolet/metas/app/ToStringApp.scala
@@ -7,6 +7,11 @@ class ToStringClass(n: Int, label: String)
 @ToString
 class ToStringClassA(n: Int, label: String)
 
+@ToString
+class HasToString(n: Int, label: String) {
+  override def toString: String = "yes"
+}
+
 private object ToStringApp extends App {
   val no = new ToStringClass(1, "non-annotated")
   println(no.toString)
