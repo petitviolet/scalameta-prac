5337f9ca79e3a2bcc4308f9fcb1ef3b47845baa0
petitviolet <violethero0820@gmail.com>
2017-02-06 08:59:34 +0900

    Point sample `+` -> `:+:`

--- modules/app/src/main/scala/net/petitviolet/metas/app/PointApp.scala
+++ modules/app/src/main/scala/net/petitviolet/metas/app/PointApp.scala
@@ -6,12 +6,12 @@ object PointApp extends App {
   val p1 = AvailablePoint(100)
   val p2 = AvailablePoint(200)
 
-  assert(Point.add(p1, p2) == p1 + p2)
+  assert(Point.add(p1, p2) == p1 :+: p2)
 
   val r1 = ReceivedPoint(200)
   val r2 = ReceivedPoint(300)
 
-  assert(Point.add(r1, r2) == r1 + r2)
+  assert(Point.add(r1, r2) == r1 :+: r2)
 
   val o1 = OtherPoint(50)
   val o2 = OtherPoint(30)
--- modules/app/src/main/scala/net/petitviolet/metas/app/macros/Point.scala
+++ modules/app/src/main/scala/net/petitviolet/metas/app/macros/Point.scala
@@ -3,11 +3,11 @@ package net.petitviolet.metas.app.macros
 sealed trait Point extends Any
 
 case class ReceivedPoint(value: Int) extends AnyVal with Point {
-  def +(other: ReceivedPoint): ReceivedPoint = ReceivedPoint(value + other.value)
+  def :+:(other: ReceivedPoint): ReceivedPoint = ReceivedPoint(value + other.value)
 }
 
 case class AvailablePoint(value: Int) extends AnyVal with Point {
-  def +(other: AvailablePoint): AvailablePoint = AvailablePoint(value + other.value)
+  def :+:(other: AvailablePoint): AvailablePoint = AvailablePoint(value + other.value)
 }
 
 case class OtherPoint(value: Int)  extends AnyVal with Point {
@@ -23,7 +23,7 @@ object Point {
 
   def addImpl[P <: Point](c: blackbox.Context)(p1: c.Expr[P], p2: c.Expr[P]): c.Expr[P] = {
     import c.universe._
-    val tree = q"$p1 + $p2"
+    val tree = q"$p1 :+: $p2"
     c.Expr[P](tree)
   }
 }
