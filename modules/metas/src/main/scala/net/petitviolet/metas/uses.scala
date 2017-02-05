78cf3f3382e60319fa03d4c41406a0f35eb7da3b
petitviolet <violethero0820@gmail.com>
2017-02-05 22:39:05 +0900

    add document

--- modules/metas/src/main/scala/net/petitviolet/metas/uses.scala
+++ modules/metas/src/main/scala/net/petitviolet/metas/uses.scala
@@ -4,18 +4,36 @@ import scala.annotation.compileTimeOnly
 import scala.collection.immutable.Seq
 import scala.meta._
 
+
 /**
- * - before
- * {{{
- *  @AddAbstractField
-    trait AddField[MyOriginalService]
- * }}}
- *
- * - after
- * {{{
- *  trait AddField[MyOriginalService] {val myOriginalService: MyOriginalService}
- * }}}
+ * inject a field has type `T`
+ * @tparam T type of a `val` to inject as a field
  */
+@compileTimeOnly("@Uses not expanded")
+class Uses[T] extends scala.annotation.StaticAnnotation {
+  inline def apply(defn: Any): Any = meta {
+    val injectionTypeOpt = this match {
+      case Term.New(Template(_, Seq(Term.Apply(
+      Term.ApplyType(_, Seq(Type.Name(typeParam: String))), _)), _, _)) =>
+        Some(typeParam)
+      case _ => None
+    }
+    defn match {
+      case cls@Defn.Trait(_, _, _, _, template) =>
+        val addField: Seq[Decl.Val] = injectionTypeOpt map { injectionType =>
+          Uses.createFieldToAdd(injectionType) :: Nil
+        } getOrElse Nil
+        val templateStats: Seq[Stat] =
+          addField ++ template.stats.getOrElse(Nil)
+
+        cls.copy(templ = template.copy(stats = Some(templateStats)))
+      case _ =>
+        println(this.structure)
+        abort("@Uses[T] can only annotate a trait")
+    }
+  }
+}
+
 object Uses {
   private[metas] def createFieldToAdd(typeName: String): Decl.Val = {
     val fields = {
@@ -32,45 +50,11 @@ object Uses {
 
 }
 
-object MixIn {
-
-  private[metas] def implementationToAdd(typeName: String, impl: Term): Defn.Val = {
-    val fieldName = {
-      val valName = typeName.head.toLower + typeName.tail
-      Pat.Var.Term(Term.Name(valName))
-    }
-    val typeAnnotation = Type.Name(typeName)
-
-    q"val $fieldName: $typeAnnotation = $impl"
-  }
-
-}
-
-
-@compileTimeOnly("@Uses not expanded")
-class Uses[T] extends scala.annotation.StaticAnnotation {
-  inline def apply(defn: Any): Any = meta {
-    val injectionTypeOpt = this match {
-      case Term.New(Template(_, Seq(Term.Apply(
-            Term.ApplyType(_, Seq(Type.Name(typeParam: String))), _)), _, _)) =>
-        Some(typeParam)
-      case _ => None
-    }
-    defn match {
-      case cls @ Defn.Trait(_, _, _, _, template) =>
-        val addField: Seq[Decl.Val] = injectionTypeOpt map { injectionType =>
-          Uses.createFieldToAdd(injectionType) :: Nil
-        }  getOrElse Nil
-        val templateStats: Seq[Stat] =
-            addField ++ template.stats.getOrElse(Nil)
-
-        cls.copy(templ = template.copy(stats = Some(templateStats)))
-      case _ =>
-        println(this.structure)
-        abort("@Uses[T] can only annotate a trait")
-    }
-  }
-}
+/**
+ * inject a field has type `T` with implementation of `T`
+ * @param impl implementation of type `T`
+ * @tparam T  type to inject as a field
+ */
 @compileTimeOnly("@MixIn not expanded")
 class MixIn[T](impl: T) extends scala.annotation.StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
@@ -93,13 +77,13 @@ class MixIn[T](impl: T) extends scala.annotation.StaticAnnotation {
     val addField = MixIn.implementationToAdd(injectionType, impl)
 
     defn match {
-      case cls @ Defn.Class(_, _, _, _, template) =>
+      case cls@Defn.Class(_, _, _, _, template) =>
         val templateStats: Seq[Stat] = addField +: template.stats.getOrElse(Nil)
         cls.copy(templ = template.copy(stats = Some(templateStats)))
-      case obj @ Defn.Object(_, _, template) =>
+      case obj@Defn.Object(_, _, template) =>
         val templateStats: Seq[Stat] = addField +: template.stats.getOrElse(Nil)
         obj.copy(templ = template.copy(stats = Some(templateStats)))
-      case trt @ Defn.Trait(_, _, _, _, template) =>
+      case trt@Defn.Trait(_, _, _, _, template) =>
         val templateStats: Seq[Stat] = addField +: template.stats.getOrElse(Nil)
         trt.copy(templ = template.copy(stats = Some(templateStats)))
       case _ =>
@@ -107,5 +91,19 @@ class MixIn[T](impl: T) extends scala.annotation.StaticAnnotation {
         abort("invalid annotating")
     }
   }
+}
+
+object MixIn {
+
+  private[metas] def implementationToAdd(typeName: String, impl: Term): Defn.Val = {
+    val fieldName = {
+      val valName = typeName.head.toLower + typeName.tail
+      Pat.Var.Term(Term.Name(valName))
+    }
+    val typeAnnotation = Type.Name(typeName)
+
+    q"val $fieldName: $typeAnnotation = $impl"
+  }
+
 }
 
