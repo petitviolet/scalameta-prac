7a80dd2c9d57429d0534a332c81f2def03e6445c
petitviolet <violethero0820@gmail.com>
2017-02-05 11:28:35 +0900

    implemented Uses[T] macro annotation to inject `val t: T`

--- modules/app/src/main/scala/net/petitviolet/metas/app/AddFieldApp.scala
+++ modules/app/src/main/scala/net/petitviolet/metas/app/AddFieldApp.scala
@@ -1,6 +1,25 @@
 package net.petitviolet.metas.app
 
+import net.petitviolet.metas.Uses
+
 //@AddField
 trait AddFieldApp[Service] {
 }
 
+trait MyService {
+  def double(n: Int) = n * 2
+}
+
+@Uses[MyService]
+trait UsesFieldTarget {
+  def show(n: Int) = println(this.myService.double(n))
+}
+
+class UsesFieldTargetImpl extends UsesFieldTarget {
+  override val myService: MyService = new MyService {}
+}
+
+object UsesFieldApp extends App {
+  val impl = new UsesFieldTargetImpl
+  impl.show(100)
+}
--- modules/metas/src/main/scala/net/petitviolet/metas/AddField.scala
+++ modules/metas/src/main/scala/net/petitviolet/metas/AddField.scala
@@ -1,5 +1,6 @@
 package net.petitviolet.metas
 
+import scala.annotation.compileTimeOnly
 import scala.collection.immutable.Seq
 import scala.meta.Decl.Val
 import scala.meta._
@@ -38,7 +39,7 @@ class AddField extends scala.annotation.StaticAnnotation {
 }
 
 object AddField {
-  def createFieldToAdd(typeName: String): Val = {
+  private[metas] def createFieldToAdd(typeName: String): Val = {
     val fields = {
       val fieldName = {
         val valName = typeName.head.toLower + typeName.tail
@@ -53,27 +54,30 @@ object AddField {
 }
 
 
-class UsesField extends scala.annotation.StaticAnnotation {
+@compileTimeOnly("@Uses not expanded")
+class Uses[T] extends scala.annotation.StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
+    println(this.structure)
+    val injectionTypeOpt = this match {
+      case Term.New(Template(_, Seq(Term.Apply(
+      Term.ApplyType(_, Seq(Type.Name(target: String))), _)), _, _)) =>
+        Some(target)
+      case _ => None
+    }
     defn match {
-      case cls@Defn.Trait(_, _, tparams, _, template) =>
-        val addField = UsesField.createFieldToAdd(classOf[String])
+      case cls @ Defn.Trait(_, _, _, _, template) =>
+        val addField: Seq[Val] = injectionTypeOpt map { injectionType =>
+          AddField.createFieldToAdd(injectionType) :: Nil
+        }  getOrElse Nil
         val templateStats: Seq[Stat] =
-          addField +: template.stats.getOrElse(Nil)
+          if (template.syntax.contains("toString")) {
+            template.stats getOrElse Nil
+          } else {
+            addField ++ template.stats.getOrElse(Nil)
+          }
 
         cls.copy(templ = template.copy(stats = Some(templateStats)))
     }
   }
 }
 
-object UsesField {
-  def createFieldToAdd[_](clazz: Class[_]) = {
-    val fieldName = {
-      val name = clazz.getSimpleName
-      val valName = name.head.toLower + name.tail
-      Pat.Var.Term(Term.Name(valName))
-    }
-    val typeName = Type.Name(clazz.getSimpleName)
-    q"val $fieldName: $typeName"
-  }
-}
