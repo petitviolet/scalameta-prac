09a30c7b243b9bd290fc7187c50cb9c1e1b2ff29
petitviolet <violethero0820@gmail.com>
2017-08-16 08:52:15 +0900

    suppress compilation errors

--- modules/app/src/main/scala/net/petitviolet/metas/app/macros/Point.scala
+++ modules/app/src/main/scala/net/petitviolet/metas/app/macros/Point.scala
@@ -3,28 +3,36 @@ package net.petitviolet.metas.app.macros
 import scala.language.experimental.macros
 import scala.language.reflectiveCalls
 
-sealed trait Point {
-  type Self = Int // >: this.type /**/>: Point
-  def :+:(other: Self): Self // = ??? // = PointOps.add[P#Self](this.asInstanceOf[P#Self], other)
-
-  //  def :+:[P <: Point](other:P): P = add(this.asInstanceOf[P], other)
-  //  def add[P <: Point](self: P, other:P): P = PointOps.add[P](self, other)
-
-  //  def :+:[P <: Point](other:P): P = PointOps.add[P](this.asInstanceOf[P], other)
-}
-
-case class ReceivedPoint(value: Int) extends Point { //self: Point.Self =>
-  //  override type Self = ReceivedPoint
-  def :+:(other: ReceivedPoint): ReceivedPoint = ReceivedPoint(value + other.value)
-
-  override def :+:(other: Self): Self = ???
-}
-
-case class AvailablePoint(value: Int) extends Point {
-  //  override type Self = AvailablePoint
-  def :+:(other: AvailablePoint): AvailablePoint = AvailablePoint(value + other.value)
-  override def :+:(other: Self): Self = ???
-}
+//sealed trait Point {
+//  def ++[P<: Self](other: P): Self
+//}
+//
+//case class AP(value: Int) extends Point {
+//  type Self >: this.type <: AP
+//  override def ++[P <: Self](other: P): AP = AP(this.value + other.value)
+//}
+//sealed trait Point {
+//  type Self = Int // >: this.type /**/>: Point
+//  def :+:(other: Self): Self// = ??? // = PointOps.add[P#Self](this.asInstanceOf[P#Self], other)
+//
+////  def :+:[P <: Point](other:P): P = add(this.asInstanceOf[P], other)
+////  def add[P <: Point](self: P, other:P): P = PointOps.add[P](self, other)
+//
+////  def :+:[P <: Point](other:P): P = PointOps.add[P](this.asInstanceOf[P], other)
+//}
+//
+//case class ReceivedPoint(value: Int) extends Point { //self: Point.Self =>
+////  override type Self = ReceivedPoint
+//  def :+:(other: ReceivedPoint): ReceivedPoint = ReceivedPoint(value + other.value)
+//
+//  override def :+:(other: Self): Self = ???
+//}
+//
+//case class AvailablePoint(value: Int) extends Point {
+////  override type Self = AvailablePoint
+//  def :+:(other: AvailablePoint): AvailablePoint = AvailablePoint(value + other.value)
+//  override def :+:(other: Self): Self = ???
+//}
 
 //case class OtherPoint(value: Int)  extends Point {
 //  def :+:(other: P): P = OtherPoint(value + other.value)
--- modules/app/src/main/scala/net/petitviolet/metas/app/macros/PointOps.scala
+++ modules/app/src/main/scala/net/petitviolet/metas/app/macros/PointOps.scala
@@ -1,24 +1,24 @@
 package net.petitviolet.metas.app.macros
 
-object PointOps {
-  import scala.language.experimental.macros
-  import scala.reflect.macros.blackbox
-
-  def add[P <: Point](p1: P, p2: P): P = macro addImpl[P]
-
-  def addImpl[P <: Point](c: blackbox.Context)(p1: c.Expr[P], p2: c.Expr[P]): c.Expr[P] = {
-    import c.universe._
-    val tree = q"$p1 :+: $p2"
-    c.Expr[P](tree)
-  }
-
-  def tm(c: blackbox.Context)(x: c.Expr[Int])(y: c.Expr[Int]) = c.universe.Ident(c.universe.TypeName("C"))
-
-  def selfType[P <: Point: c.WeakTypeTag](c: blackbox.Context): c.universe.Ident = {
-    import c.universe._
-    val typeName = weakTypeOf[P].typeSymbol.fullName
-    Ident(TermName(typeName))
-  }
-}
+//object PointOps {
+//  import scala.language.experimental.macros
+//  import scala.reflect.macros.blackbox
+//
+//  def add[P <: Point](p1: P, p2: P): P = macro addImpl[P]
+//
+//  def addImpl[P <: Point](c: blackbox.Context)(p1: c.Expr[P], p2: c.Expr[P]): c.Expr[P] = {
+//    import c.universe._
+//    val tree = q"$p1 :+: $p2"
+//    c.Expr[P](tree)
+//  }
+//
+//  def tm(c: blackbox.Context)(x: c.Expr[Int])(y: c.Expr[Int]) = c.universe.Ident(c.universe.TypeName("C"))
+//
+//  def selfType[P <: Point: c.WeakTypeTag](c: blackbox.Context): c.universe.Ident = {
+//    import c.universe._
+//    val typeName = weakTypeOf[P].typeSymbol.fullName
+//    Ident(TermName(typeName))
+//  }
+//}
 
 
