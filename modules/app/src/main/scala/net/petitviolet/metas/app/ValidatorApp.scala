fe7c553ff24e9acfc60a37cb0d2dc84bd1dbc491
petitviolet <violethero0820@gmail.com>
2017-08-15 09:48:11 +0900

    fixed ValidatorApp

--- modules/app/src/main/scala/net/petitviolet/metas/app/ValidatorApp.scala
+++ modules/app/src/main/scala/net/petitviolet/metas/app/ValidatorApp.scala
@@ -2,9 +2,13 @@ package net.petitviolet.metas.app
 
 import net.petitviolet.metas.validator._
 
+
 object NonNullApp extends App {
   @NonNull
-  val v1 = 100 // OK
+  def v1: Int = {
+    val x = 100
+    x
+  }// OK
 
   @NonNull
   def d1(): Null = {
@@ -13,49 +17,49 @@ object NonNullApp extends App {
   }
 
   @NonNull
-  val v2 = {
+  def v2 = {
     //    null // error
   }
 
   @NonNull
-  lazy val v3 = null
+  def v3 = null
 
   //  println(d1) // error
   //  println(v3) // error
 }
 
 object SizeApp extends App {
-  @Length(min = 1, max = 10)
-  val seq = 1 to 10 // OK
+  @Length(1, 10)
+  def seq = 1 to 10 // OK
 
   @Length(min = 10)
-  val seq2 = 1 to 10 // OK
+  def seq2 = 1 to 10 // OK
 
   @Length(max = 10)
-  val seq3 = 1 to 10 // OK
+  def seq3 = 1 to 10// OK
 
-  @Length(min = 20, max = 100)
-  val seq4 = 1 to 10 // Error
+  @Length(1, 100)
+  def seq4 = 1 to 10
 }
 
 object NonEmptyApp extends App {
   @NonEmpty
-  val nonEmtpy = 1 :: Nil
+  def nonEmtpy = { List(1, 2, 3) }
 
   @NonEmpty
-  val empty = Nil // Error
+  def empty = Nil // Error
 }
 
 object RangeApp extends App {
-  @IntRange(min = 1, max = 10)
-  val ok1 = 5
-
-  @IntRange(max = 10)
-  val ok2 = 5
-
-  @IntRange(min = 1)
-  val ok3 = 5
-
-  @IntRange(min = 1, max = 10)
-  val ng = 100
+//  @IntRange(min = Some(1), max = Some(10))
+//  def ok1 = 5
+//
+//  @IntRange(max = Some(10))
+//  def ok2 = 5
+//
+//  @IntRange(min = Some(1))
+//  def ok3 = 5
+//
+//  @IntRange(min = Some(1), max = Some(10))
+//  def ng = 100
 }
