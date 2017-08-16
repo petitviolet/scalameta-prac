32e112a7cc368e86d1a95224b8694c553c646800
petitviolet <violethero0820@gmail.com>
2017-08-17 08:17:06 +0900

    fixed @TimeLogging annotation

--- modules/app/src/main/scala/net/petitviolet/metas/app/TimeLogging.scala
+++ modules/app/src/main/scala/net/petitviolet/metas/app/TimeLogging.scala
@@ -6,16 +6,18 @@ import scala.meta._
 @compileTimeOnly("logging not expanded")
 class TimeLogging extends StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
-    val getNano = q"System.nanoTime()"
+    val getNano: Term = q"System.nanoTime()"
     defn match {
       case d @ Defn.Def(_, name, _, _, _, body) =>
+        def s = Lit.String.apply _
         val newBody =
           q"""
-          val methodName = ${name.value}
+          val methodName = ${s(name.value)}
           val start = $getNano
           val result = $body
           val end = $getNano
-          println(s"[$$methodName] tracking time: $${(end - start) / 1000000} ms")
+          val millis = (end - start) / ${Lit.Long(1000000)}
+          println(${s("[")} + methodName + ${s("]tracking time: ")} + millis + ${s(" ms")})
           result
           """
         d.copy(body = newBody)
