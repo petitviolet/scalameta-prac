5377ba2c02a063e761d45c9796e7c2386dfb168a
petitviolet <violethero0820@gmail.com>
2017-08-23 08:18:16 +0900

    simplified sample @hello

--- modules/metas/src/main/scala/net/petitviolet/metas/hello.scala
+++ modules/metas/src/main/scala/net/petitviolet/metas/hello.scala
@@ -6,10 +6,12 @@ import scala.meta._
 @compileTimeOnly("hello")
 class hello extends StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
-    val hello = q"""println(${Lit.String("hello")})"""
     defn match {
       case d: Defn.Def =>
-        val newBody = q"{$hello; ${d.body}}"
+        val newBody = q"""{
+          println("hello")
+          ${d.body}
+        }"""
         d.copy(body = newBody)
       case _ =>
         abort("annotate only def")
