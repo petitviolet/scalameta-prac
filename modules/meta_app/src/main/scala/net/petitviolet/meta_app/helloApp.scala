3aec93b4f89928ee34afbc79936ed1a63c4b9fec
petitviolet <violethero0820@gmail.com>
2017-08-28 08:38:11 +0900

    add MetaApp for use scala.meta at runtime

--- modules/meta_app/src/main/scala/net/petitviolet/meta_app/helloApp.scala
+++ modules/meta_app/src/main/scala/net/petitviolet/meta_app/helloApp.scala
@@ -3,6 +3,7 @@ package net.petitviolet.meta_app
 import net.petitviolet.metas.hello
 
 object helloApp extends App {
+
   @hello
   def say() = {
     println("world")
@@ -10,3 +11,9 @@ object helloApp extends App {
 
   say()
 }
+
+object MetaApp extends App {
+  import scala.meta._
+  println(Term.Name("x"))
+}
+
